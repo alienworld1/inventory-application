69bd692bc3b69625254603df3294d8c51a3ec1d4
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-02 20:40:23 +0530

    Add item model

---
+++ models/items.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  description: {type: String, required: true},
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  price: {
+    type: mongoose.Schema.Types.Decimal128,
+    required: true,
+    min: 0,
+  },
+  number_in_stock: {
+    type: Number,
+    min: 0,
+    default: 0, 
+  },
+});
+
+ItemSchema.virtual('url').get(function() {
+  return `/inventory/item/${this._id}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
