fbe114fc558b6d30d47ee074764cd54381576b82
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-04 12:27:13 +0530

    Add routes and controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,35 @@
+const asyncHandler = require('express-async-handler');
+
+const Category = require('../models/categories');
+
+exports.category_list = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category list');
+});
+
+exports.category_detail = asyncHandler(async(req, res, next) => {
+  res.send(`Not implemented: category detail: ${req.params.id}`);
+});
+
+exports.category_create_get = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category create GET');
+});
+
+exports.category_create_post = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category create POST');
+});
+
+exports.category_delete_get = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category delete GET');
+});
+
+exports.category_delete_post = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category delete POST');
+});
+
+exports.category_update_get = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category update GET');
+});
+
+exports.category_update_post = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: category update POST');
+});
---
+++ controllers/itemController.js
@@ -0,0 +1,40 @@
+const asyncHandler = require('express-async-handler');
+
+const Item = require('../models/items');
+const Category = require('../models/categories');
+
+exports.index = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: inventory home page');
+});
+
+exports.item_list = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item list');
+});
+
+exports.item_detail = asyncHandler(async(req, res, next) => {
+  res.send(`Not implemented: Item detail: ${req.params.id}`);
+});
+
+exports.item_create_get = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item create GET');
+});
+
+exports.item_create_post = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item create POST');
+});
+
+exports.item_delete_get = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item delete GET');
+});
+
+exports.item_delete_post = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item delete POST');
+});
+
+exports.item_update_get = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item update GET');
+});
+
+exports.item_update_post = asyncHandler(async(req, res, next) => {
+  res.send('Not implemented: item update POST');
+});
