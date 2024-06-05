ec988e4d51d4a618c1fb0f13976878eb48ea5352
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-05 20:54:32 +0530

    Add a form to create a category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 const Category = require('../models/categories');
 
@@ -24,12 +25,45 @@ exports.category_detail = asyncHandler(async(req, res, next) => {
 });
 
 exports.category_create_get = asyncHandler(async(req, res, next) => {
-  res.send('Not implemented: category create GET');
+  res.render('category_form', {title: 'Create Category'});
 });
 
-exports.category_create_post = asyncHandler(async(req, res, next) => {
-  res.send('Not implemented: category create POST');
-});
+exports.category_create_post = [
+  body('name', 'Category name must be specified')
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body('description', 'Description must contain at least 5 characters')
+    .trim()
+    .isLength({min: 5})
+    .escape(),
+  
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({name: req.body.name, description: req.body.description});
+
+    if (!errors.isEmpty()) {
+      res.render('category_form', {
+        title: 'Create Category',
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const categoryExists = await Category.findOne({name: req.body.name})
+        .collation({locale: 'en', strength: 2})
+        .exec();
+      
+      if (categoryExists) {
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
 
 exports.category_delete_get = asyncHandler(async(req, res, next) => {
   res.send('Not implemented: category delete GET');
