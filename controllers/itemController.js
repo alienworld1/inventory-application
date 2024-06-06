e2d54c26f30ba372733ac024072ec780698c4e83
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-06 21:43:32 +0530

    Add a create item form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 const Item = require('../models/items');
 const Category = require('../models/categories');
@@ -41,12 +42,65 @@ exports.item_detail = asyncHandler(async(req, res, next) => {
 });
 
 exports.item_create_get = asyncHandler(async(req, res, next) => {
-  res.send('Not implemented: item create GET');
+  const categories = await Category.find({}, 'name').sort({name : 1}).exec();
+
+  res.render('item_form', {title: 'Create item', categories: categories});
 });
 
-exports.item_create_post = asyncHandler(async(req, res, next) => {
-  res.send('Not implemented: item create POST');
-});
+exports.item_create_post = [
+  body('name', 'Item name must be specified')
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body('description', 'Description must contain at least 5 characters')
+    .trim()
+    .isLength({min: 5})
+    .escape(),
+  body('category', 'Category must be specified')
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body('price')
+    .trim()
+    .isLength({min: 1})
+    .withMessage('Price must be specified')
+    .isFloat({gt: 0})
+    .withMessage('Price must be a positive value')
+    .toFloat(),
+  body('number_in_stock')
+    .trim()
+    .isLength({min: 1})
+    .withMessage('Number in stock must be specified')
+    .isInt({gt: 0})
+    .withMessage('Number in stock must be a positive integer')
+    .toInt(),
+  
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+    });
+
+    if (!errors.isEmpty()) {
+      const categories = await Category.find({}, 'name').sort({name: 1}).exec();
+      res.render('item_form', {
+        title: 'Create item',
+        categories: categories,
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
 
 exports.item_delete_get = asyncHandler(async(req, res, next) => {
   res.send('Not implemented: item delete GET');
